f43d4a7270985eb10063496ffaef6b7f9c939243
Ahmed-Reda0 <68137755+Ahmed-Reda-Dev@users.noreply.github.com>
2023-04-12 14:04:31 +0200

    `chatGPT`

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,3 +1,3 @@
 // ignore_for_file: non_constant_identifier_names
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
